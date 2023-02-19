ef61ee8e68828072aee9d6bcdbd4227a263a23e1
Dmitry Vasiliuk <usy222@mail.ru>
2023-02-19 21:36:05 +0300

    add Task 1

---
+++ Task1/Program.cs
@@ -0,0 +1,52 @@
+﻿// Программа, которая на вход принимает координаты точки (Х, У), причем Х не = 0 и У не = 0,
+// и выдает номер четверти плоскости в которой находится эта точка.
+while (true)
+{
+  Task();
+}
+
+void Task()
+{
+  int x = ReadInt("X");
+  int y = ReadInt("Y");
+
+  int quater = GetQuarter (x, y);
+
+  Console.WriteLine($"{quater}: [{x} - {y}]");
+}
+
+int GetQuarter(int x, int y)
+{ 
+  if (x == 0 || y == 0)
+  {
+    return -1;
+  }
+
+  if (x > 0 && y > 0)
+  {
+    return 1;
+  }
+  else if (x < 0 && y > 0)
+  {
+    return 2;
+  }
+  else if (x < 0 && y < 0)
+  {
+    return 3;
+  }
+  else
+  {
+    return 4;
+  }
+}
+
+int ReadInt(string argumentName)      // создаем функцию
+{
+  Console.Write($"Input {argumentName}: ");
+  int number = 0;
+  while (!int.TryParse(Console.ReadLine(),  out number))
+  {
+    Console.WriteLine("It's not a number");
+  }
+  return number;
+}
