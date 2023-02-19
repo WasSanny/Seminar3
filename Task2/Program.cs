c26777fb4c7c712ecfb8ec888175d33c9eb3b5bf
Dmitry Vasiliuk <usy222@mail.ru>
2023-02-19 23:04:58 +0300

    add Task 2

---
+++ Task2/Program.cs
@@ -0,0 +1,33 @@
+﻿// See Программа, которая по заданному номеру четверти,
+// показывает диапозон возможных координат точек
+// в этой четверти (x, y)
+
+Console.OutputEncoding = System.Text.Encoding.UTF8; // для знака
+Console.InputEncoding = System.Text.Encoding.UTF8;  // бесконечности!
+
+Console.WriteLine("Введите номер четверти: ");
+int GetQuerter = int.Parse(Console.ReadLine());
+
+int[] x = new int[] {1, -1, -1, 1};
+int[] y = new int[] {1, 1, -1, -1};
+
+if (GetQuerter == 1)
+{
+  Console.WriteLine($"X от {x[0]} до +∞");
+  Console.WriteLine($"Y от {y[0]} до +∞");
+}
+else if (GetQuerter == 2)
+{
+  Console.WriteLine($"X от {x[1]} до -∞");
+  Console.WriteLine($"Y от {y[1]} до +∞");
+}
+else if (GetQuerter == 3)
+{
+  Console.WriteLine($"X от {x[2]} до -∞");
+  Console.WriteLine($"Y от {y[2]} до -∞");
+}
+else if (GetQuerter == 4)
+{
+  Console.WriteLine($"X от {x[3]} до +∞");
+  Console.WriteLine($"Y от {y[3]} до -∞");
+}
